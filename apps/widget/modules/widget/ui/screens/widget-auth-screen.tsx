b00d615bccce319c6b4abd80a6db1d19feed10a3
nikhilsrivaw <nikhilksrivastav190@gmail.com>
2025-09-13 16:36:43 +0530

    widget-sessions

---
+++ apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
@@ -0,0 +1,129 @@
+import { WidgetHeader } from "../components/widget-header"
+import {Form , FormControl , FormField , FormItem , FormMessage,} from "@workspace/ui/components/form"
+import { Button } from "@workspace/ui/components/button"
+import {Input} from  "@workspace/ui/components/input"
+import {useForm} from "react-hook-form";
+import {z} from "zod"
+import {zodResolver} from "@hookform/resolvers/zod"
+import { useMutation } from "convex/react";
+import {api} from "@workspace/backend/_generated/api"
+import { Doc } from "@workspace/backend/_generated/dataModel"
+
+//temproroary adding  a id 
+const organizationId = "123"
+const formSchema = z.object({
+    name : z.string().min(1,"Name is required "),
+    email: z.string().email("Inavlid email address"),
+
+})
+
+
+
+
+export const WidgetAuthScreen = ()=>{
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver : zodResolver(formSchema),
+        defaultValues:{
+            name : "",
+            email:""
+        }
+    })
+
+    const createContactSession = useMutation(api.public.contactSessions.create);
+
+
+
+    const onSubmit = async (values:z.infer<typeof formSchema>)=>{
+        if(!organizationId){
+            return;
+        }
+        //@ts-ignore
+        const metadata: Doc<"contactSessions">["metadata"]={
+            userAgent: navigator.userAgent,
+            language : navigator.language,
+            languages : navigator.languages?.join(","),
+            platform: navigator.platform,
+            vendor:navigator.vendor,
+            screenResolution:`${screen.width}x&{screen.height}`,
+            viewportSize:`${window.innerWidth}x&{window.innerHeight}`,
+            timezone : Intl.DateTimeFormat().resolvedOptions().timeZone,
+            timezoneOffset : new Date().getTimezoneOffset(),
+            cookieEnabled: navigator.cookieEnabled,
+            referrer: document.referrer || "direct",
+            currentUrl : window.location.href
+
+            
+        };
+
+        const contactSessionId = await createContactSession({
+            ...values,
+            organizationId,
+            metadata
+        });
+
+        console.log({ contactSessionId})
+
+    };
+
+
+    return (
+        <>
+            
+                <WidgetHeader>
+                <div className = " flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
+                    <p className = 'text-3xl '>
+                        Hi there!
+                    </p>
+                    <p className="text-large">
+                        Lets get you started 
+                    </p>
+
+
+                </div>
+               </WidgetHeader>
+               <Form {...form}>
+                <form className="flex flex-1 flex-col gap-y-4 p-4" onSubmit={form.handleSubmit(onSubmit)} >
+                    
+                    <FormField name="name"  control = {form.control} render={({ field})=>(
+                        <FormItem>
+                            <FormControl>
+                                <Input
+                                  className="h-10 bg-background"
+                                  placeholder="e.g. Nikhil "
+                                  type ="text"
+                                  {...field}
+                                
+                                />
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+
+                    )}/>
+                    <FormField name="email"  control = {form.control} render={({ field})=>(
+                        <FormItem>
+                            <FormControl>
+                                <Input
+                                  className="h-10 bg-background"
+                                  placeholder="e.g. Nikhil@example.com "
+                                  type ="email"
+                                  {...field}
+                                
+                                />
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                        
+                    )}/>
+
+                    <Button disabled={form.formState.isSubmitting} size='lg' type ="submit">Continue</Button>
+
+
+                </form>
+               </Form>
+
+            
+        
+        </>
+    )
+
+}

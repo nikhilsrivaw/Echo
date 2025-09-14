d4cf1b9df9f5e1582dec2a078989ca725eb9a812
nikhilsrivaw <nikhilksrivastav190@gmail.com>
2025-09-14 22:08:44 +0530

    conversations

---
+++ packages/backend/convex/public/conversations.ts
@@ -0,0 +1,67 @@
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "../_generated/server";
+import { threadId } from "worker_threads";
+  
+
+export const getOne = query({
+    args:{
+        conversationId : v.id("conversations"),
+        contactSessionId: v.id("contactSessions")
+    },
+    handler: async (ctx , args)=>{
+        const session = await  ctx.db.get(args.contactSessionId);
+
+        if(!session || session.expiresAt < Date.now()){
+            throw new ConvexError({
+                code:"UNAUTHORIZED",
+                message : "Invalid sesson "
+            });
+
+        }
+
+        const conversation = await ctx.db.get(args.conversationId);
+
+        if(!conversation){
+            return null;
+        }
+
+        return {
+            _id: conversation._id,
+            status: conversation.status,
+            threadId: conversation.threadId
+        }
+
+
+    }
+})
+export const create = mutation({
+    args:{
+        organizationId : v.string(),
+        contactSessionId : v.id("contactSessions"),
+
+    },
+    handler: async( ctx , args)=>{
+        const session = await ctx.db.get(args.contactSessionId);
+        if(!session  || session.expiresAt <Date.now()){
+            throw new  ConvexError({
+                code: " UNAUTHORIZED",
+                message : " Invalid session"
+
+            })
+
+        }
+        //TODO : replace once functionalty for thread creation is prsent 
+        const threadId = "123";
+
+
+        const conversationId = await ctx.db.insert("conversations" , {
+            contactSessionId: session._id,
+            status: "unresolved",
+            organizationId : args.organizationId,
+            threadId,
+        });
+
+        return conversationId;
+
+    }
+})
--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -3,6 +3,24 @@ import { v } from "convex/values";
 
 
 export default defineSchema({
+    conversations:defineTable({
+        threadId : v.string(),
+        organizationId :v.string(),
+        contactSessionId : v.id("contactSessions"),
+        status :v.union(
+            v.literal("unresolved"),
+            v.literal("esclated"),
+            v.literal("resolved")
+
+        )
+
+        
+
+    }).index("by_organization_id" , ["organizationId"])
+    .index("by_contact_session_id" , ["contactSessionId"])
+    .index("by_thread_id" , ["threadId"])
+    .index("by_status_and_organization_id" , ["status" , "organizationId"])
+    ,
     contactSessions:defineTable({
         name: v.string(),
         email: v.string(),
@@ -31,3 +49,5 @@ export default defineSchema({
     })
 
 })
+
+
